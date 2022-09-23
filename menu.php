d5a91ae0b562b1422499c44019c7b48c76b8fde4
Patrick Chicaiza <patricioalexanderitq@gmail.com>
2022-09-22 22:58:20 -0500

    cambio datos meta en menu.php

--- menu.php
+++ menu.php
@@ -8,9 +8,10 @@ if (!isset($_SESSION['idUsuario'])) {
 
 <html>
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name=viwport content="width=device-width", initial-scale=1.0>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
 
 
