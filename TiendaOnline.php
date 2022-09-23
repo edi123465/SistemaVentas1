4747e8f74ed3fee879070fdce79a8a2ce9bd77a7
Edison Vinicio <edison.borja100@gmail.com>
2022-09-22 22:21:26 -0500

    CAMBIOS REALIZADOS AL ARCHIVO TIENDAONLINE.PHP

--- TiendaOnline.php
+++ TiendaOnline.php
@@ -10,5 +10,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     </head>
     <body>
         <h1>Tienda del sistema</h1>
+        <p>Estos cambios son realizados desde el usuario 2</p>
     </body>
 </html>
